6878747b51d4538f5bb70ef684130b2f3d64740c
Dellan Muchengapadare <dellan.muchengapadare@platform45.com>
2024-10-15 16:34:39 +0200

    add snippets to nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,81 +4,89 @@
 -- See the kickstart.nvim README for more information
 return {
   {
-    "max397574/better-escape.nvim",
+    'max397574/better-escape.nvim',
     lazy = false,
     config = function()
-      require("better_escape").setup {
-        mapping = { "jj" }, -- a table with mappings to use
-        timeout = 200,      -- the time in which the keys must be hit in ms. clear_empty_lines = false, keys = '<ESC>', -- keys used for escaping
+      require('better_escape').setup {
+        mapping = { 'jj' }, -- a table with mappings to use
+        timeout = 200, -- the time in which the keys must be hit in ms. clear_empty_lines = false, keys = '<ESC>', -- keys used for escaping
       }
-    end
+    end,
   },
-  { "tpope/vim-rails", ft = { "ruby" } },
+  { 'tpope/vim-rails', ft = { 'ruby' } },
   {
-    "mactunechy/rails-view-component-gd.nvim",
+    'mactunechy/rails-view-component-gd.nvim',
     ft = { 'slim', 'erb', 'rb' },
     keys = {
-      { "<leader>gd", "<cmd>OpenViewComponentClass <CR>",    desc = "ViewComponent Class" },
-      { "<leader>gk", "<cmd>OpenViewComponentTemplate <CR>", desc = "ViewComponent Template" },
-      { "<leader>gs", "<cmd>OpenViewComponentSpec <CR>",     desc = "ViewComponent Spec" },
-      { "<leader>gv", "<cmd>ToggleViewComponent <CR>",       desc = "Toggle ViewComponent ClassAndTemplate" },
+      { '<leader>gd', '<cmd>OpenViewComponentClass <CR>', desc = 'ViewComponent Class' },
+      { '<leader>gk', '<cmd>OpenViewComponentTemplate <CR>', desc = 'ViewComponent Template' },
+      { '<leader>gs', '<cmd>OpenViewComponentSpec <CR>', desc = 'ViewComponent Spec' },
+      { '<leader>gv', '<cmd>ToggleViewComponent <CR>', desc = 'Toggle ViewComponent ClassAndTemplate' },
     },
     config = function()
       require('rails-view-component-gd').setup()
-    end
+    end,
   },
   {
-    "nathom/filetype.nvim",
+    'nathom/filetype.nvim',
     lazy = false,
     config = function()
-      require("filetype").setup({
+      require('filetype').setup {
         overrides = {
           extensions = {
-            slim = "slim"
-          }
-        }
-      })
-    end
+            slim = 'slim',
+          },
+        },
+      }
+    end,
   },
   {
-    "slim-template/vim-slim",
-    ft = "slim",
+    'slim-template/vim-slim',
+    ft = 'slim',
   },
   {
-    "kdheepak/lazygit.nvim",
+    'kdheepak/lazygit.nvim',
     cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
     },
     dependencies = {
-      "nvim-lua/plenary.nvim",
+      'nvim-lua/plenary.nvim',
     },
     keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
   },
-  { "preservim/vimux", event = 'VeryLazy' }, -- send test command in tmux pane
+  { 'preservim/vimux', event = 'VeryLazy' }, -- send test command in tmux pane
   {
-    "vim-test/vim-test",
+    'vim-test/vim-test',
     event = 'VeryLazy',
     keys = {
-      { '<leader>tt', '<cmd>TestNearest -strategy=neovim<CR>', desc = "Test Nearest" },
-      { '<leader>tl', '<cmd>TestLast -strategy=neovim<CR>',    desc = "Test Last" },
-      { '<leader>tf', '<cmd>TestFile -strategy=neovim<CR>',    desc = "Test File" },
-      { '<leader>ts', '<cmd>TestSuite -strategy=neovim<CR>',   desc = "Test Suite" },
+      { '<leader>tt', '<cmd>TestNearest -strategy=neovim<CR>', desc = 'Test Nearest' },
+      { '<leader>tl', '<cmd>TestLast -strategy=neovim<CR>', desc = 'Test Last' },
+      { '<leader>tf', '<cmd>TestFile -strategy=neovim<CR>', desc = 'Test File' },
+      { '<leader>ts', '<cmd>TestSuite -strategy=neovim<CR>', desc = 'Test Suite' },
     },
     config = function()
-      vim.cmd([[ let test#ruby#rspec#executable = 'spring rspec' ]])
-    end
+      vim.cmd [[ let test#ruby#rspec#executable = 'bundle exec rspec' ]]
+    end,
   },
-{
-    "almo7aya/openingh.nvim",
-    event = "VeryLazy",
+  {
+    'almo7aya/openingh.nvim',
+    event = 'VeryLazy',
     keys = {
-      { "<leader>og", "<cmd>OpenInGHFileLines<cr>", desc = "Open in Github" },
+      { '<leader>og', '<cmd>OpenInGHFileLines<cr>', desc = 'Open in Github' },
     },
+  },
+
+  {
+    'L3MON4D3/LuaSnip',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    config = function()
+      require('luasnip').filetype_extend('ruby', { 'rails' })
+    end,
   },
 }
